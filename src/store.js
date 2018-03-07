7e4e643ac77d6e7d5360784aec4f394cde37d66f
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-03-07 12:15:19 -0800

    Finished challange

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
+import { createStore, combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import { deliveryReducer } from './deliveryReducer';
 
-import {createStore, combineReducers} from 'redux'
-import {reducer as formReducer} from 'redux-form'
-import {deliveryReducer} from './deliveryReducer';
+const reducer = combineReducers({
+  form: formReducer // mounted under "form"
+});
+const store = (window.devToolsExtension
+  ? window.devToolsExtension()(createStore)
+  : createStore)(reducer);
 
-export default createStore(
-    combineReducers({
-        form: formReducer,
-        delivery: deliveryReducer
-    })
-);
-
+export default store;
